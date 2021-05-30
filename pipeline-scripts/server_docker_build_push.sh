69140d2605f80949fd3e0d3307f0cec2eabb3bc1
Ashwin P M <ashwin.pm@cardinalhealth.com>
2021-05-30 16:28:09 +0530

    changes

--- pipeline-scripts/server_docker_build_push.sh
+++ pipeline-scripts/server_docker_build_push.sh
@@ -2,7 +2,9 @@
 
 cd server
 echo -n $ARTIFACTORY_CRED_PSW | docker login --username $ARTIFACTORY_CRED_USR --password-stdin $DOCKER_BASE
-docker image build . --tag "$DOCKER_BASE/$DOCKER_ACCOUNT_NAME/$SERVER_IMAGE:$SERVER_TAG"
+docker image build . \
+    --tag "$DOCKER_BASE/$DOCKER_ACCOUNT_NAME/$SERVER_IMAGE:$SERVER_TAG" \
+    --no-cache
 docker image push "$DOCKER_BASE/$DOCKER_ACCOUNT_NAME/$SERVER_IMAGE:$SERVER_TAG"
 docker image rm "$DOCKER_BASE/$DOCKER_ACCOUNT_NAME/$SERVER_IMAGE:$SERVER_TAG"
 docker logout
